ea4caf5cd846f9714e480cb1d28aa2355f3ebba7
1kurs1 <lmtk161044@gmail.com>
2023-10-12 23:04:56 +0300

    Added load-music button!

--- headers/core/stdinclude.hpp
+++ headers/core/stdinclude.hpp
@@ -2,5 +2,12 @@
 
 #include <iostream>
 
-#define GLFW_INCLUDE_VULKAN
+#include "../imgui/imgui.h"
+#include "../imgui/imgui_impl_glfw.h"
+#include "../imgui/imgui_impl_opengl3.h"
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 640;
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
-#include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include "core/stdinclude.hpp"
 
 int main(int, char**){
     if(!glfwInit()) return -1;
 
-    GLFWwindow* window = glfwCreateWindow(800, 640, "MusicPlayer", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "MusicPlayer", NULL, NULL);
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    // Setup ImGUI:
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //ImVec2 windowSize = {WINDOW_WIDTH, WINDOW_HEIGHT};
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_NewFrame();
+
+    // Setup style:
+    ImGui::StyleColorsDark();
+
+    ImVec4 clearColor = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
 
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "couldn't load OpenGL!" << '\n';
@@ -18,10 +30,33 @@ int main(int, char**){
 
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+
+        ImGui::NewFrame();
+
+        ImGui::SetNextWindowSize(ImVec2(WINDOW_WIDTH, WINDOW_HEIGHT));
+        if(ImGui::Begin("Test", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse)){
+            if(ImGui::Button("Load Music", ImVec2(100, 50))){
+
+            }
+        }
+        ImGui::End();
+        ImGui::Render();
+        ImGui::EndFrame(); 
+
+        int displayWidth, displayHeight;
+        glfwGetFramebufferSize(window, &displayWidth, &displayHeight);
+        glViewport(0, 0, displayWidth, displayHeight);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
+    // Cleanup:
     glfwTerminate();
+
     return EXIT_SUCCESS;
 }
